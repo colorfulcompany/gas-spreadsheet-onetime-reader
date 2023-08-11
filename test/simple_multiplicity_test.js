195df9fb5f8f06795c45a5986d9c7689410b91de
T.Watanabe <watanabe@colorfulcompany.co.jp>
2023-08-11 12:40:47 +0900

    test: add test for complex data

--- test/simple_multiplicity_test.js
+++ test/simple_multiplicity_test.js
@@ -9,11 +9,29 @@ const app = gas.require('./src', {
 
 describe('SimpleMultiplicity', () => {
   describe('#multiplicities', () => {
-    it('return object {value: multiplicity, value: multiplicity, ...}', () => {
-      assert.deepEqual(
-        { 1: 1, 2: 3, 3: 2, 4: 1 },
-        app.createMultiplicity([1, 2, 3, 3, 4, 2, 2]).multiplicities()
-      )
+    describe('simple array of number', () => {
+      it('return object {value: multiplicity, value: multiplicity, ...}', () => {
+        assert.deepEqual(
+          { 1: 1, 2: 3, 3: 2, 4: 1 },
+          app.createMultiplicity([1, 2, 3, 3, 4, 2, 2]).multiplicities()
+        )
+      })
+    })
+
+    describe('complex array', () => {
+      it('count mutiplicity as json', () => {
+        assert.deepEqual(
+          {
+            '{"key1":"a","key2":[1,2]}': 2,
+            '{"key1":"a","key2":[2,3]}': 1
+          },
+          app.createMultiplicity([
+            { key1: 'a', key2: [1, 2] },
+            { key1: 'a', key2: [2, 3] },
+            { key1: 'a', key2: [1, 2] }
+          ]).multiplicities()
+        )
+      })
     })
   })
 
